3a6ade277324d29e04976fbc452c63d078b2f6f0
Veer Singh <a.veersingh.me@gmail.com>
2023-09-22 11:32:35 -0700

    Done

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,12 +4,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
+  private TalonFX right2;
+
   //add motors here
   public ArmSubsystem() {
-    //make proper initializations
+		right2 = new TalonFX(CanIds.rightFalcon2.id);
+		
+		right2.configFactoryDefault();
+		right2.setInverted(true);
   }
 
-  //consider what methods are necessary for the motor to run effectively
+  public TalonFX getRightFollow() {
+    return right2;
+  }
+
+
+  public void setRightPower(double power) {
+      right2.set(power);
+  }
   
   @Override
   public void periodic() {
